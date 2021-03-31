d7b50eea29a4d0abc047e5be4c45c19da044d0e6
CLightning <0xc3phalex1n@gmail.com>
2021-03-31 18:52:34 +0430

    Update Main.cpp

--- Runtime Obfuscation/Runtime Obfuscation/Main.cpp
+++ Runtime Obfuscation/Runtime Obfuscation/Main.cpp
@@ -10,10 +10,8 @@ BASE g_CallGate = { 0 };
 
 int main(int argc, char* argv[])
 {
-	obfuscation::load(g_CallGate);
+	obfuscation::load();
 	g_CallGate._MessageBoxW(NULL, L"I am an anonymous call.", L"Obfuscated Runtime", MB_OKCANCEL);
 
 	return 0;
 }
-
-
